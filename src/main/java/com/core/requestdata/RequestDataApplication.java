750bfe3c18ad2e92cd651980f8fc74bcded47813
Andres Leotur <leoturorellana@gmail.com>
2022-05-19 20:54:07 -0400

    Inciando el proyecto request-data

---
+++ src/main/java/com/core/requestdata/RequestDataApplication.java
@@ -0,0 +1,13 @@
+package com.core.requestdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/core/requestdata/RequestDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.requestdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
